7f81550818f1f8b6a467eb235722aa4e1af26476
Moshimota <dasercasseigzez@gmail.com>
2022-12-30 13:15:27 +0500

    fix

---
+++ main.js
@@ -0,0 +1,15 @@
+function hello() {
+	return "hello edabit.com"
+}
+
+function addition(a, b) {
+	return (a+b)
+}
+
+function convert(minutes) {
+	return (minutes*60)
+}
+
+function addition(num) {
+	return (num+1)
+}
